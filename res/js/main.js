4f92d79d56d3df0d3d9b07e78052725ce14ff5ee
Nabeel Rasheed <nrasheed@accertify.com>
2019-01-04 15:15:08 -0600

    layout, w3css

--- res/js/main.js
+++ res/js/main.js
@@ -2,6 +2,12 @@ function test() {
   alert("testing...");
 }
 
+function itemClicked(ref) {
+  var id = ref.getAttribute("data-id");
+  alert("item clicked:" + id);
+  console.log("item clicked:" + id);
+}
+
 function playSound(id) {
   document.querySelector('#' + id + "_mp3").play();
 }
